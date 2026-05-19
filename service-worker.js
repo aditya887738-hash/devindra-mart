9d101116d55c32bab412aef7df23196ffd644700
aditya887738-hash <aditya887738@gmail.com>
2026-05-19 12:46:32 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE='dm-live-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/')))));
